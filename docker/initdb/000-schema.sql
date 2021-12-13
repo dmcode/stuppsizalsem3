6073d087c57b9a0c615928b95e50d53df4fa6275
Dominik Łukasik <dmcode@outlook.com>
2021-12-13 21:18:41 +0100

    Konfiguracja środowiska deweloperskiego
    Podstawowa konfiguracja środowiska
    
    Konfiguracja php
    
    Poprawki w konfiguracji
    
    Zmiana na PHP 8.1
    
    Build context dla kontenera php

---
+++ docker/initdb/000-schema.sql
@@ -0,0 +1,4 @@
+create table user (
+    id int unsigned auto_increment primary key,
+    username varchar(100) not null
+);
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<h1>Hello <?php echo $_SERVER['SERVER_NAME']?>!</h1>
+
+<?php phpinfo();
